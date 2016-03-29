3bfae7a82c017f773b072f373543e9639b461ef7
AyrFX <oleg@gbg.bg>
2016-03-29 23:16:11 +0300

    Added tasks from 11 to 16

---
+++ 01.Intro-Programming-Homework/15.Age/Age.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Age
+{
+    static void Main()
+    {
+        string birthdayAsString = Console.ReadLine();
+        DateTime birthday;
+        DateTime.TryParse(birthdayAsString, out birthday);
+        DateTime currentDate = DateTime.Today;
+        int age = currentDate.Year - birthday.Year;
+        if (currentDate.AddYears(-age) < birthday)
+        {
+            age--;
+        }
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
+    }
+}
