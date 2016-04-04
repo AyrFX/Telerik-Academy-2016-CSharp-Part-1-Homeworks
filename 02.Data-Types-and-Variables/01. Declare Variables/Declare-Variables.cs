74f1e9f3453bfbd7019a4d243d0e781b2074ccac
AyrFX <oleg@gbg.bg>
2016-04-04 22:54:42 +0300

    Added Data Types and Variables Solution and Task 1

---
+++ 02.Data-Types-and-Variables/01. Declare Variables/Declare-Variables.cs
@@ -0,0 +1,11 @@
+﻿class DeclareVariables
+{
+    static void Main()
+    {
+        ushort ushortNumber = 52130;
+        sbyte sbyteNumber = -115;
+        long longNumber = 4825932L;
+        byte byteNumber = 97;
+        short shortNumber = -10000;
+    }
+}
