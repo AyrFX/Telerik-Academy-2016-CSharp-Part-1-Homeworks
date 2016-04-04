9da5c0addebc3ab95458b4a83b25a612a0fe1df3
AyrFX <oleg@gbg.bg>
2016-04-04 23:08:12 +0300

    Added Task 2 from Data Types and Variables Homework

---
+++ 02.Data-Types-and-Variables/02. Float or Double/Float-or-Double.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class FloatOrDouble
+{
+    static void Main()
+    {
+        double doubleNumber = 34.567839023D;
+        Console.WriteLine(doubleNumber);
+        float floatNumber = 12.345F;
+        Console.WriteLine(floatNumber);
+        doubleNumber = 8923.1234857F;
+        Console.WriteLine(doubleNumber);
+        floatNumber = 3456.091F;
+        Console.WriteLine(floatNumber);
+    }
+}
