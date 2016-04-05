ff66e1f568e54d47915a61d94fdfad3422552401
AyrFX <oleg@gbg.bg>
2016-04-05 21:46:18 +0300

    Added Task 9 to Data Types and Variables Homework

---
+++ 02.Data-Types-and-Variables/09. Exchange Variable Values/ExchangeVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class ExchangeVariables
+{
+    static void Main()
+    {
+        int a = 5, b = 10;
+        Console.WriteLine("a = " + a + "; b = " + b);
+        int c = a;
+        a = b;
+        b = c;
+        Console.WriteLine("a = " + a + "; b = " + b);
+    }
+}
