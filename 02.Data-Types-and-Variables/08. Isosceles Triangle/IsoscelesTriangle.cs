150015c71b3eb01725620efd4ab879e04e4ae7b3
AyrFX <oleg@gbg.bg>
2016-04-05 21:40:38 +0300

    Added Task 8 to Data Types and Variables Homework

---
+++ 02.Data-Types-and-Variables/08. Isosceles Triangle/IsoscelesTriangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("   ©");
+        Console.WriteLine();
+        Console.WriteLine("  © ©");
+        Console.WriteLine();
+        Console.WriteLine(" ©   ©");
+        Console.WriteLine();
+        Console.WriteLine("© © © ©");
+    }
+}
