d532d34e5eaadcb5b18ab176755faa4fd368d9b4
AyrFX <oleg@gbg.bg>
2016-04-05 23:38:30 +0300

    Added Task 4 to Operators and Expressions Homework

---
+++ 03.Operators-and-Expressions/04. Rectangles/Rectangles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Rectangels
+{
+    static void Main()
+    {
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+        Console.WriteLine(area.ToString("0.00") + " " + perimeter.ToString("0.00"));
+    }
+}
