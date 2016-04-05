37a5de1219713a90f3c2179da23ae5969862444a
AyrFX <oleg@gbg.bg>
2016-04-05 23:10:08 +0300

    Added Task 1 to Operators and Expressions Homework

---
+++ 03.Operators-and-Expressions/01. Odd or Even/OddOrEven.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class OddOrEven
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+        if (inputNumber % 2 == 0)
+        {
+            Console.WriteLine("even " + inputNumber);
+        }
+        else
+        {
+            Console.WriteLine("odd " + inputNumber);
+        }
+    }
+}
