a473ff51852b0751ba620cf8b809dfe63f4ac31f
AyrFX <oleg@gbg.bg>
2016-04-05 21:57:50 +0300

    Added Task 10 to Data Types and Variables Homework

---
+++ 02.Data-Types-and-Variables/10. Employee Data/EmployeeData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class EmployeeData
+{
+    static void Main()
+    {
+        string firstName = "Pesho";
+        string lastName = "Goshev";
+        byte age = 27;
+        bool isFemale = false;
+        long personalId = 123123123;
+        int uniqueNumber = 27563474;
+        Console.WriteLine("First name: " + firstName);
+        Console.WriteLine("Last name: " + lastName);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Female: " + isFemale);
+        Console.WriteLine("Personal ID: " + personalId);
+        Console.WriteLine("Unique Number: " + uniqueNumber);
+    }
+}
