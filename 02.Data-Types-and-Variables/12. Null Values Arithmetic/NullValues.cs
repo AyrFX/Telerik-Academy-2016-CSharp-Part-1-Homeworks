c1ed6c95384b30dde0b68946be7fbcb3cd4c99b0
AyrFX <oleg@gbg.bg>
2016-04-05 22:28:24 +0300

    Added Task 12 to Data Types and Variables Homework

---
+++ 02.Data-Types-and-Variables/12. Null Values Arithmetic/NullValues.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class NullValues
+{
+    static void Main()
+    {
+        int? integerNumber = null;
+        double? doubleNumber = null;
+        Console.WriteLine(integerNumber);
+        Console.WriteLine(doubleNumber);
+        integerNumber += 5;
+        doubleNumber += 0.12;
+        Console.WriteLine(integerNumber);
+        Console.WriteLine(doubleNumber);
+        integerNumber = 5;
+        doubleNumber = 0.12;
+        Console.WriteLine(integerNumber);
+        Console.WriteLine(doubleNumber);
+    }
+}
