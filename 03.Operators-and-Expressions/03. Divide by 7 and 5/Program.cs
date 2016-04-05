7293a263b0fb8ee885255a13d2e4a5445b49cd6c
AyrFX <oleg@gbg.bg>
2016-04-05 23:27:03 +0300

    Added Task 3 to Operators and Expressions Homework

---
+++ 03.Operators-and-Expressions/03. Divide by 7 and 5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int inputNumber = int.Parse(Console.ReadLine());
+        if (inputNumber % 5 == 0 && inputNumber % 7 == 0)
+        {
+            Console.WriteLine("true " + inputNumber);
+        }
+        else
+        {
+            Console.WriteLine("false " + inputNumber);
+        }
+    }
+}
