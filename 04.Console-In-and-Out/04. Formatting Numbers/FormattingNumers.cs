17736ad5b4e62c4c608173b44173adb6a1f2d21d
AyrFX <oleg@gbg.bg>
2016-04-15 23:08:41 +0300

    Corrected binary representation of numbers in Task 4 of ConsoleIO Homework

--- 04.Console-In-and-Out/04. Formatting Numbers/FormattingNumers.cs
+++ 04.Console-In-and-Out/04. Formatting Numbers/FormattingNumers.cs
@@ -7,6 +7,6 @@ class FormattingNumers
         int numberA = int.Parse(Console.ReadLine());
         float numberB = float.Parse(Console.ReadLine());
         float numberC = float.Parse(Console.ReadLine());
-        Console.WriteLine("{0,-1:X3} | {1:0000000000} | {2,3:0.00} | {3,-5:0.000} |", numberA, numberA, numberB, numberC);
+        Console.WriteLine("{0,-1:X3}|{1}|{2,3:0.00}|{3,-5:0.000}|", numberA, Convert.ToString(numberA, 2), numberA, numberB, numberC);
     }
 }
