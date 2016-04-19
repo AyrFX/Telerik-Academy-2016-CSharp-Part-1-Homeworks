7c5c72bbc7a43b194433363612853517d13ed1a0
AyrFX <oleg@gbg.bg>
2016-04-19 21:59:05 +0300

    Added directory structure for Conditional Statement Homework and Task 1

---
+++ 05. Conditional-Statements/01. Exchange If Greater/ExchangeIfGreather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ExchangeIfGreather
+{
+    static void Main()
+    {
+        double numberA = double.Parse(Console.ReadLine());
+        double numberB = double.Parse(Console.ReadLine());
+        double greaterNumber;
+        if (numberA > numberB)
+        {
+            greaterNumber = numberA;
+            numberA = numberB;
+            numberB = greaterNumber;
+        }
+        Console.WriteLine("{0} {1}", numberA, numberB);
+    }
+}
