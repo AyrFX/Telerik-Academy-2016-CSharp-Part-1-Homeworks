0e03a302d6b3b20dc2e32936126fc44059a08e2f
AyrFX <oleg@gbg.bg>
2016-04-19 22:10:55 +0300

    Added Task 2 of Conditional Statements Homework

---
+++ 05. Conditional-Statements/02. Bonus Score/BonusScore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class BonusScore
+{
+    static void Main()
+    {
+        short score = short.Parse(Console.ReadLine());
+        if (1 <= score && score <= 3)
+        {
+            score *= 10;
+            Console.WriteLine(score);
+        }
+        else if (4 <= score && score <= 6)
+        {
+            score *= 100;
+            Console.WriteLine(score);
+        }
+        else if (7 <= score && score <= 9)
+            {
+            score *= 1000;
+            Console.WriteLine(score);
+        }
+        else
+        {
+            Console.WriteLine("invalid score");
+        }
+    }
+}
