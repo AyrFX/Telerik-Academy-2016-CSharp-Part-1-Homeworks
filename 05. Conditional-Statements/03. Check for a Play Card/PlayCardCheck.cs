2291201dd2e73bd85beac6577b0c240d59423c75
AyrFX <oleg@gbg.bg>
2016-04-19 22:24:20 +0300

    Added Task 3 of Conditional Statement Homework

---
+++ 05. Conditional-Statements/03. Check for a Play Card/PlayCardCheck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class PlayCardCheck
+{
+    static void Main()
+    {
+        string inputCard = Console.ReadLine();
+        switch (inputCard)
+        {
+            case "2":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "3":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "4":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "5":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "6":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "7":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "8":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "9":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "10":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "J":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "Q":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "K":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            case "A":
+                Console.WriteLine("yes " + inputCard);
+                break;
+            default:
+                Console.WriteLine("no " + inputCard);
+                break;
+        }
+    }
+}
