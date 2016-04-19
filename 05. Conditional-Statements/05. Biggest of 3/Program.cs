76fafc0ae7371fe575afd5b31e70bb395694338a
AyrFX <oleg@gbg.bg>
2016-04-19 22:57:17 +0300

    Added Task 5 of Conditional Statements Homework

---
+++ 05. Conditional-Statements/05. Biggest of 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double numberA = double.Parse(Console.ReadLine());
+        double numberB = double.Parse(Console.ReadLine());
+        double numberC = double.Parse(Console.ReadLine());
+        if (numberA > numberB && numberA > numberC)
+        {
+            Console.WriteLine(numberA);
+        }
+        else if (numberB > numberA && numberB > numberC)
+        {
+            Console.WriteLine(numberB);
+        }
+        else
+        {
+            Console.WriteLine(numberC);
+        }
+    }
+}
