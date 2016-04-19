a3ab25c5d1e21b9b1f0a67805060ebd6cab11c2d
AyrFX <oleg@gbg.bg>
2016-04-19 22:48:34 +0300

    Added Task 4 of Conditional Statements Homework

---
+++ 05. Conditional-Statements/04. Multiplication Sign/MultiplocationSign.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class MultiplocationSign
+{
+    static void Main()
+    {
+        double numberA = double.Parse(Console.ReadLine());
+        double numberB = double.Parse(Console.ReadLine());
+        double numberC = double.Parse(Console.ReadLine());
+        if (numberA == 0 || numberB == 0 || numberC == 0)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+        if (numberA > 0)
+        {
+            if (numberB > 0)
+            {
+                if (numberC > 0)
+                {
+                    Console.WriteLine("+");
+                }
+                else
+                {
+                    Console.WriteLine("-");
+                }
+            }
+            else
+            {
+                if (numberC > 0)
+                {
+                    Console.WriteLine("-");
+                }
+                else
+                {
+                    Console.WriteLine("+");
+                }
+            }
+        }
+        else
+        {
+            if (numberB > 0)
+            {
+                if (numberC > 0)
+                {
+                    Console.WriteLine("-");
+                }
+                else
+                {
+                    Console.WriteLine("+");
+                }
+            }
+            else
+            {
+                if (numberC > 0)
+                {
+                    Console.WriteLine("+");
+                }
+                else
+                {
+                    Console.WriteLine("-");
+                }
+            }
+        }
+    }
+}
